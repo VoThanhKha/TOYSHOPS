967344db339cf80be25e64927574db7e38955845
Khavtgcc200092 <Khavtgcc200092@fpt.edu.vn>
2022-10-18 10:33:48 +0700

    Try

--- header.php
+++ header.php
@@ -41,7 +41,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-            <a class="nav-link" href="index.php">Homee</a>
+            <a class="nav-link" href="index.php">Home</a>
             </li>
             <li class="nav-item">
             <a class="nav-link" href="cart.php">Cart</a>
