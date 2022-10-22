ad0ff3835139c1ec6461a1af1ea899008677b471
Khavtgcc200092 <Khavtgcc200092@fpt.edu.vn>
2022-10-22 08:31:33 +0700

    demo

--- connect.php
+++ connect.php
@@ -11,9 +11,9 @@
 // }
 
 
-	$conn = pg_connect("localhost","root","","shop_200092") or die("Lỗi".pg_connect($conn));
+	// $conn = pg_connect("localhost","root","","shop_200092") or die("Lỗi".pg_connect($conn));
 	
-	pg_connect($conn,'SET NAMES "utf8"');
+	// pg_connect($conn,'SET NAMES "utf8"');
 	//mysqli_close($Connect);*/
 	$conn = pg_pconnect("postgres://evjwdctyczrqow:65c77aa39bff6486959ad4e9532e62b27234a6693e6611c442a980e99d133448@ec2-34-235-198-25.compute-1.amazonaws.com:5432/dfcio4ifhhi5ik");
     //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
