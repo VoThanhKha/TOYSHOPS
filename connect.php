d21c76f3113ddebaccdc5be197b6f58ebede8831
Khavtgcc200092 <Khavtgcc200092@fpt.edu.vn>
2022-10-22 09:09:34 +0700

    demo

--- connect.php
+++ connect.php
@@ -15,7 +15,7 @@
 	
 	// pg_connect($conn,'SET NAMES "utf8"');
 	//mysqli_close($Connect);*/
-	$conn = pg_pconnect("postgres://evjwdctyczrqow:65c77aa39bff6486959ad4e9532e62b27234a6693e6611c442a980e99d133448@ec2-34-235-198-25.compute-1.amazonaws.com:5432/dfcio4ifhhi5ik");
+	$conn = pg_connect("postgres://evjwdctyczrqow:65c77aa39bff6486959ad4e9532e62b27234a6693e6611c442a980e99d133448@ec2-34-235-198-25.compute-1.amazonaws.com:5432/dfcio4ifhhi5ik");
     //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
 	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
 	
